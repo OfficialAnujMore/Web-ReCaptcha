9613493837cb3fd9d137e79c4506ff64ffa9bce5
OfficialAnujMore <moreanuj1307@gmail.com>
2024-10-25 13:27:27 -0700

    Integrated recaptcha

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,29 +2,65 @@ import React, { useState } from "react";
 import "./App.css";
 import CustomInput from "./components/CustomInput";
 import CustomButton from "./components/CustomButton";
+import ReCAPTCHA from "react-google-recaptcha";
+// import 'dotenv/config'
 
 function App() {
-  const [inputValue, setInputValue] = useState<string>("");
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-  };
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+  const [token,setToken] = useState('');
+  function onChange(recaptchaToken:any) {
+    console.log("Captcha value:", recaptchaToken);
+    setToken(recaptchaToken)
+  }
 
   const handleButtonClick = () => {
-    alert(`You entered: ${inputValue}`);
+    console.log(formData, token)
+
   };
   return (
     <div className="App">
       <CustomInput
         type="text"
-        placeholder="Enter text"
-        value={inputValue}
-        onChange={handleInputChange}
+        placeholder="Enter name"
+        value={formData.fullName}
+        onChange={(event)=>{
+          const value = event.target.value
+          setFormData((prevData) => ({
+            ...prevData,
+            fullName: value,
+          }));
+        }}
       />
-
-      <CustomButton
-        onClick={handleButtonClick}
-        label="Submit"
+      <CustomInput
+        type="text"
+        placeholder="Enter email"
+        value={formData.email}
+        onChange={(event)=>{
+          const value = event.target.value
+          setFormData((prevData) => ({
+            ...prevData,
+            email: value,
+          }));
+        }}
       />
+      <CustomInput
+        type="text"
+        placeholder="Enter password"
+        value={formData.password}
+        onChange={(event)=>{
+          const value = event.target.value
+          setFormData((prevData) => ({
+            ...prevData,
+            password: value,
+          }));
+        }}
+      />
+      <ReCAPTCHA sitekey={'6LeZWGsqAAAAAKNfKYOckZCzCabYAyZMqHKJUy8j'} onChange={onChange}/>,
+      <CustomButton onClick={handleButtonClick} label="Submit" disabled={!token} />
     </div>
   );
 }
